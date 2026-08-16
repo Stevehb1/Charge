d43f740cbd8d29ae807163b67da4074459827696
Stevehb1 <steve@hen-boisen.com>
2026-08-16 14:01:20 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const C = "charge-v2";
+
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c => c.addAll(["./", "./index.html"])).catch(() => {}));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== C).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(C).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match("./index.html")))
+  );
+});
